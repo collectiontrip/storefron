92d4ceb49e974d4f6008c3525df800dd5c0375fc
collectiontrip <rdochania92@gmail.com>
2024-06-12 11:04:18 +0530

    Add django debug toolbar and ad first view

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def say_hy(request):
-    return HttpResponse("Hello World")
+def say_hello(request ):
+    x = 1
+    y = 2
+    return render(request, "hello.html", { 'name': 'Ravi'})
+    
